84f280137321dc77eb5da03d717a8806ffc36ebd
codebr3ak <barakaralph@gmail.com>
2023-08-27 16:24:34 +0300

    Almost done with the form

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,18 @@
 
 function sendEmail(){
   sendEmail.send({
-    Host : "smtp.elasticemail.com",
-    Username : "username",
-    Password : "password",
-    To : 'them@website.com',
-    From : "you@isp.com",
+    Host : "smtp.gmail.com",
+    Username : "barakaralph@gmail.com",
+    Password : "",
+    To : 'barakaralph@gmail.com',
+    From : document.getElementById("email").value,
     Subject : "This is the subject",
-    Body : "And this is the body"
+    Body : "Name:" + document.getElementById("name").value
+    + "<br> Email: " + document.getElementById("email").value
+    + "<br> Phone no: " + document.getElementById("phone").value
+    + "<br> Message: " + document.getElementById("message").value
   }).then(
-    message => alert(message)
+    message => alert("Message sent successfully")
   )
 };
 
